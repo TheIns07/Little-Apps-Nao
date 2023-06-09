5a8b1f96fc75d5dd4e480f2f6b4bf137f187e23b
TheIns07 <arturoinscreup2401@hotmail.com>
2023-06-08 19:56:19 -0700

    Detalles de impresion de datos

--- nao/src/components/Cycle1/AudioTexto.tsx
+++ nao/src/components/Cycle1/AudioTexto.tsx
@@ -9,10 +9,16 @@ const AudioTexto = () => {
         <div>
             {hasRecognitionSupport ? (
                 <>
+                <div>
+                    <button onClick={startListening}>Empezar</button>
+                </div>
+                {isListeling ? <div>Escuchando...</div>: null}
+                {text}
                 </>
 
             ) :
                 <>
+                <div>No hay soporte</div>
                 </>
             }
         </div>
